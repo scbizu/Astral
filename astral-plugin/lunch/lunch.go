940363f836e0ea37a34a2b3963c588c9465dc246
scnace <scbizu@gmail.com>
2017-08-05 18:41:44 +0800

    first init

---
+++ astral-plugin/lunch/lunch.go
@@ -0,0 +1,167 @@
+package lunch
+
+import (
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/scbizu/wechat-go/wxweb"
+)
+
+const (
+	receiver = ""
+	//SPACE defines 4 space globally
+	SPACE = "    "
+	//ENTER defines enter globally
+	ENTER = "\n"
+)
+
+var (
+	lunching, taking bool
+
+	membersCount int32
+	//fromUserID => userinfo
+	members map[string]*wxweb.User
+
+	owner string
+	// mmChannel chan map[string]*wxweb.User
+)
+
+//Register regist the lunch plugin to the bot
+func Register(session *wxweb.Session, lunchFunc func(session *wxweb.Session, msg *wxweb.ReceivedMessage)) {
+	if lunchFunc == nil {
+		lunchFunc = defaultLunch
+	}
+	session.HandlerRegister.Add(wxweb.MSG_TEXT, wxweb.Handler(lunchFunc), "callon")
+	if err := session.HandlerRegister.EnableByName("callon"); err != nil {
+		log.Println(err)
+	}
+
+}
+
+func addmember() {}
+
+func deletemember() {}
+
+func defaultLunch(session *wxweb.Session, msg *wxweb.ReceivedMessage) {
+
+	if !msg.IsGroup {
+		return
+	}
+	var contact *wxweb.User
+	orderList := []int{}
+	if msg.FromUserName == session.Bot.UserName {
+		contact = session.Cm.GetContactByUserName(msg.ToUserName)
+	} else {
+		contact = session.Cm.GetContactByUserName(msg.FromUserName)
+	}
+	if contact == nil {
+		return
+	}
+	mm, err := wxweb.CreateMemberManagerFromGroupContact(session, contact)
+	if err != nil {
+		log.Panicln(err)
+		return
+	}
+	whoCall := mm.GetContactByUserName(msg.Who)
+	msg.Content = strings.TrimSpace(msg.Content)
+	if strings.HasPrefix(msg.Content, "/") {
+		//Astral的饭团模式开启
+		rawCommand := strings.TrimPrefix(msg.Content, "/")
+		switch rawCommand {
+		case "lunch":
+			if lunching {
+				session.SendText("lauching,if it was already ended,plz type /end", session.Bot.UserName, msg.FromUserName)
+			} else {
+				lunching = true
+				session.SendText(whoCall.NickName+" created a new lunch,type /join to join in.And type /take to be the lunch owner", session.Bot.UserName, msg.FromUserName)
+			}
+		case "take":
+			if !lunching {
+				session.SendText("type /new to new lunch first", session.Bot.UserName, msg.FromUserName)
+			} else if owner != "" {
+				session.SendText("sorry,the owner is "+owner, session.Bot.UserName, msg.FromUserName)
+			} else {
+				taking = true
+				owner = msg.FromUserName
+				session.SendText(whoCall.NickName+" now is the luncher owner.Type `/random ?`OR `/manual (?,?,?)`(type /baocan to check the index of the menu) to order, e.g: `/random 10` OR `/manual (1,2,3)`", session.Bot.UserName, msg.FromUserName)
+			}
+
+		case "baocan":
+			orderListStr := getAllRecipeInfo()
+			session.SendText(orderListStr, session.Bot.UserName, msg.FromUserName)
+		case "send":
+			if taking && msg.FromUserName == owner {
+				session.SendText("您好,我们点菜,12点左右过来吃。菜单是:\n "+showRecipe(orderList)+"\n 麻烦了,O(∩_∩)O谢谢~", session.Bot.UserName, receiver)
+				session.SendText("recipe was sent,type /star (?) to star this recipe after lunch.", session.Bot.UserName, msg.FromUserName)
+				//reset
+				taking = false
+			} else {
+				session.SendText("WTF!Dont kidding me!", session.Bot.UserName, msg.FromUserName)
+			}
+		case "star":
+			if lunching && msg.FromUserName == owner {
+				star := strings.TrimPrefix(rawCommand, "star")
+				session.SendText("The recipe is "+star+" ✨,it will work next time.type /end to end.", session.Bot.UserName, msg.FromUserName)
+				owner = ""
+			} else {
+				session.SendText("WTF!Dont kidding me!", session.Bot.UserName, msg.FromUserName)
+			}
+		case "join":
+			if !lunching {
+				session.SendText("no lunch now,type /lunch to create a new lunch", session.Bot.UserName, msg.FromUserName)
+			} else {
+				// membersCount = atomic.AddInt32(&membersCount, 1)
+				membersCount = membersCount + 1
+				session.SendText(whoCall.NickName+" joined in,now has "+strconv.Itoa(int(membersCount))+" members", session.Bot.UserName, msg.FromUserName)
+			}
+		case "end":
+			if !lunching {
+				session.SendText("already ended", session.Bot.UserName, msg.FromUserName)
+			} else {
+				session.SendText("lunch ended", session.Bot.UserName, msg.FromUserName)
+				lunching = false
+				membersCount = 0
+			}
+		default:
+			if strings.Contains(rawCommand, "manual") {
+				if taking && msg.FromUserName == owner {
+					// println(rawCommand)
+					params := strings.TrimPrefix(rawCommand, "manual(")
+					params = strings.TrimSuffix(params, ")")
+					println(params)
+					orders := strings.Split(params, ",")
+					for _, o := range orders {
+						oi, err := strconv.ParseInt(o, 10, 64)
+						if err != nil {
+							log.Println(err)
+							session.SendText("ERROR!What are U fucking typing! plz re-type it!", session.Bot.UserName, msg.FromUserName)
+						}
+						orderList = append(orderList, int(oi))
+					}
+					orderListStr := showRecipe(orderList)
+					session.SendText(orderListStr, session.Bot.UserName, msg.FromUserName)
+				} else {
+					session.SendText("WTF!Dont kidding me!", session.Bot.UserName, msg.FromUserName)
+				}
+			} else if strings.Contains(rawCommand, "random") {
+				if taking && msg.FromUserName == owner {
+					params := strings.TrimSpace(strings.TrimPrefix(rawCommand, "random"))
+					count, err := strconv.ParseInt(params, 10, 64)
+					if err != nil {
+						log.Println(err)
+						session.SendText("ERROR!What are U fucking typing! plz re-type it!", session.Bot.UserName, msg.FromUserName)
+					}
+					orderListsName := getRecipe(int(count))
+					if len(orderListsName) > 0 {
+						session.SendText(showRecipeByName(orderListsName)+"\n type /send to send this order to BAOCAN", session.Bot.UserName, msg.FromUserName)
+					}
+				} else {
+					session.SendText("WTF!Dont kidding me!", session.Bot.UserName, msg.FromUserName)
+				}
+			} else {
+				session.SendText("WTF!", session.Bot.UserName, msg.FromUserName)
+			}
+		}
+	}
+}
---
+++ astral-plugin/lunch/orderlist.go
@@ -0,0 +1,133 @@
+package lunch
+
+import (
+	"io/ioutil"
+	"log"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/go-yaml/yaml"
+)
+
+//Recipe ...
+type Recipe struct {
+	Name  string   `yaml:"name"`
+	Price int      `yaml:"price"`
+	Star  int      `yaml:"star"`
+	Tag   []string `yaml:"tag"`
+}
+
+//Restaurant ...
+type Restaurant struct {
+	Baocan []Recipe `yaml:"baocan"`
+}
+
+func getRecipe(count int) (orderLists []string) {
+	r := loadData()
+	meatCount := count / 2
+	meat := getTagRecipe(r.Baocan, "meat")
+	sortmeat := sortByStar(meat)
+	vege := getTagRecipe(r.Baocan, "vege")
+	sortvege := sortByStar(vege)
+	//not enough meat
+	if len(sortmeat) < meatCount {
+		meatCount = len(sortmeat)
+		for _, sr := range sortmeat {
+			orderLists = append(orderLists, sr.Name)
+		}
+		//not enough vege
+		if len(sortvege) < count-meatCount {
+			for _, sr := range sortvege {
+				orderLists = append(orderLists, sr.Name)
+			}
+		} else {
+			for i := 0; i < count-meatCount; i++ {
+				orderLists = append(orderLists, sortvege[i].Name)
+			}
+		}
+	} else if len(sortvege) < count-meatCount {
+		for _, sr := range sortvege {
+			orderLists = append(orderLists, sr.Name)
+		}
+		if len(sortmeat) < count-len(sortvege) {
+			for _, sr := range sortmeat {
+				orderLists = append(orderLists, sr.Name)
+			}
+		} else {
+			for i := 0; i < count-len(sortvege); i++ {
+				orderLists = append(orderLists, sortmeat[i].Name)
+			}
+		}
+	} else {
+		for i := 0; i < meatCount; i++ {
+			orderLists = append(orderLists, sortmeat[i].Name)
+		}
+		for i := 0; i < count-meatCount; i++ {
+			orderLists = append(orderLists, sortvege[i].Name)
+		}
+	}
+	return
+}
+
+func showRecipe(orders []int) (showRecipeStrs string) {
+	r := loadData()
+	for idx, recipe := range r.Baocan {
+		for _, order := range orders {
+			if idx == order {
+				showRecipeStrs = showRecipeStrs + strconv.Itoa(idx) + " : " + recipe.Name + SPACE + " ¥" + strconv.Itoa(recipe.Price) + ENTER
+			}
+		}
+	}
+	return
+}
+
+func showRecipeByName(orders []string) (showRecipeStrs string) {
+	if len(orders) > 0 {
+		showRecipeStrs = showRecipeStrs + "Your Order List:" + ENTER
+	}
+	ordersStr := strings.Join(orders, "\n")
+	showRecipeStrs = showRecipeStrs + ordersStr
+	return
+}
+
+func getAllRecipeInfo() (showRecipeStrs string) {
+	r := loadData()
+	if len(r.Baocan) > 0 {
+		showRecipeStrs = showRecipeStrs + "Order List: " + ENTER
+	}
+	for idx, recipe := range r.Baocan {
+		showRecipeStrs = showRecipeStrs + strconv.Itoa(idx) + " : " + recipe.Name + SPACE + " ¥" + strconv.Itoa(recipe.Price) + SPACE + " ✨" + strconv.Itoa(recipe.Star) + ENTER
+	}
+	return
+}
+
+func loadData() *Restaurant {
+	data, err := ioutil.ReadFile("./astral-plugin/lunch/recipe.yaml")
+	if err != nil {
+		log.Println(err)
+	}
+	r := new(Restaurant)
+	err = yaml.Unmarshal(data, r)
+	if err != nil {
+		log.Println(err)
+	}
+	return r
+}
+
+func getTagRecipe(re []Recipe, tag string) (res []Recipe) {
+	for _, r := range re {
+		tagStr := strings.Join(r.Tag, ",")
+		if strings.Contains(tagStr, tag) {
+			res = append(res, r)
+		}
+	}
+	return
+}
+
+func sortByStar(recipes []Recipe) []Recipe {
+	sort.Slice(recipes, func(i int, j int) bool {
+		return recipes[i].Star > recipes[j].Star
+	})
+	return recipes
+}
