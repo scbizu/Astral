16ec791bcd6fe464fbd78b1b533cf84a34c412b8
scnace <scbizu@gmail.com>
2018-01-07 17:46:17 +0800

    remove redis dependency

--- telegram/telegram.go
+++ telegram/telegram.go
@@ -2,35 +2,23 @@ package telegram
 
 import (
 	"errors"
-	"log"
-	"sync"
+	"os"
 
-	"github.com/go-redis/redis"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
-	"github.com/scbizu/Astral/storage"
 )
 
 const (
-	tokenKey    = "tg_token"
+	tokenKey    = "BOTKEY"
 	tgAPIDomain = "https://scnace.cc:443/"
 )
 
 var (
 	errEmptyArgs = errors.New("plz input args")
-	redisOnce    sync.Once
 	token        string
 )
 
 func init() {
-	var redisClient *redis.Client
-	redisOnce.Do(func() {
-		redisClient = storage.NewRedisClient()
-	})
-	var err error
-	token, err = redisClient.Get(tokenKey).Result()
-	if err != nil {
-		log.Fatalln(err)
-	}
+	token = os.Getenv(tokenKey)
 }
 
 func connectTG() (bot *tgbotapi.BotAPI, err error) {
