410e8c5b25e441cc884f0708dc6d928f519990e8
scnace <scbizu@gmail.com>
2018-01-15 02:29:57 +0800

    tmp remove sayhi

--- astral-plugin/plugin.go
+++ astral-plugin/plugin.go
@@ -5,7 +5,6 @@ import (
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/scbizu/Astral/astral-plugin/lunch"
-	"github.com/scbizu/Astral/astral-plugin/sayhi"
 	"github.com/scbizu/Astral/astral-plugin/today-anime"
 	"github.com/scbizu/wechat-go/wxweb"
 )
@@ -19,10 +18,10 @@ func RegisterWechatEnabledPlugins(session *wxweb.Session) {
 
 //RegistTGEnabledPlugins regists telegram plugin
 func RegistTGEnabledPlugins(rawmsg *tgbotapi.Message) (msg tgbotapi.MessageConfig) {
-	msg = sayhi.Register(rawmsg)
-	if checkMarkedMsg(msg) {
-		return
-	}
+	// msg = sayhi.Register(rawmsg)
+	// if checkMarkedMsg(msg) {
+	// 	return
+	// }
 	msg = anime.Register(rawmsg)
 	return
 }
